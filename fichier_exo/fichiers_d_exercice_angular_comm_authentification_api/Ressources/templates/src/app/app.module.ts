20bc4d9f53df590b29b953076a1bb66258dd0b62
NoePigeau <npigeau@myges.fr>
2021-07-21 13:00:54 +0200

    oui

---
+++ fichier_exo/fichiers_d_exercice_angular_comm_authentification_api/Ressources/templates/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+import { AppComponent } from './app.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { HeaderComponent } from './header/header.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactsComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
